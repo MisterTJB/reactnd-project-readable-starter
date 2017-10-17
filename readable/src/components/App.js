a104a9b2413717ac2a67069b977786c19238a65b
Tim <tim.bathgate@gmail.com>
2017-10-17 15:43:30 +1300

    Add routing and dummy reducer

---
+++ readable/src/components/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
+import reducers from '../reducers'
+
+import Home from './Home';
+
+const store = createStore(reducers);
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <BrowserRouter>
+          <div>
+          <Route path="/" component={Home} />
+          </div>
+        </BrowserRouter>
+      </Provider>
+    )
+  }
+}
+
+export default App;
